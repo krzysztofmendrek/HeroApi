0b75cb997b247d7ecf338d5656e017403711ba19
Krzysztof Mendrek <kmendrek94@gmail.com>
2020-07-31 08:03:46 +0200

    Added assets, added styles to the components

--- src/requests.js
+++ src/requests.js
@@ -6,6 +6,5 @@ export const getBasicHeroInfoById = async id => {
 
     const { data: powerstats } = await axios.get(`https://superheroapi.com/api/${ACCESS_TOKEN}/${id}/powerstats`)
     const { data: image } = await axios.get(`https://superheroapi.com/api/${ACCESS_TOKEN}/${id}/image`)
-    // console.log(image, powerstats, powerstats.name)
     return { name: powerstats.name , powerstats, imgUrl: image.url }
 }
