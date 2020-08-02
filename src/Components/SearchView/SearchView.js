6ec57ba214b548d501e80a7d0664f46c7e6ebc20
Krzysztof Mendrek <kmendrek94@gmail.com>
2020-08-02 21:06:14 +0200

    Added loader

--- src/Components/SearchView/SearchView.js
+++ src/Components/SearchView/SearchView.js
@@ -3,12 +3,15 @@ import './SearchView.css';
 import { searchHeroesByName } from '../../requests';
 import { useParams } from 'react-router';
 import HeroSimplified from '../HeroSimplified/HeroSimplified';
+import Loader from '../Loader/Loader';
 
 function SearchView() {
   const [ searchList,  setSearchListContent ] = useState([]);
+  const [ isLoading,  setLoadingState ] = useState(true);
   const { name } = useParams();
 
   useEffect(() => {
+    setLoadingState(true);
     searchHeroesByName(name).then(searchResults => {
       const { data } = searchResults;
 
@@ -18,13 +21,20 @@ function SearchView() {
 
       const { results } = data;
       setSearchListContent(results);
+      setLoadingState(false);
     })
   }, [name]);
 
   return (
-    <section className='searchView'>
-      {searchList.map(({ powerstats, image, name }) => <HeroSimplified powerstats={powerstats} imgUrl={image.url} name={name} />)}
-    </section>
+    <>
+    { !isLoading && (
+      <section className='searchView'>
+        {searchList.map(({ powerstats, image, name }) => <HeroSimplified powerstats={powerstats} imgUrl={image.url} name={name} />)}
+      </section>)
+      } {
+      isLoading && <Loader />
+      }
+    </>
   )
 };
 
