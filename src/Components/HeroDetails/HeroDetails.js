6d746981fce646952a1580505d650b9922e51a36
Krzysztof Mendrek <kmendrek94@gmail.com>
2020-08-17 08:06:16 +0200

    Work with HeroDetails

--- src/Components/HeroDetails/HeroDetails.js
+++ src/Components/HeroDetails/HeroDetails.js
@@ -5,7 +5,7 @@ import './HeroDetails.css';
 import Loader from '../Shared/Loader/Loader';
 
 function HeroDetails() {
-  const [searchHeroDetails,  setSearchHeroDetailsContent] = useState([]);
+  const [searchHeroDetails,  setSearchHeroDetailsContent] = useState();
   const [isLoading,  setLoadingState] = useState(true);
   const { id } = useParams();
 
@@ -13,36 +13,43 @@ function HeroDetails() {
     setLoadingState(true);
     getHeroDetailsById(id).then(heroDetails => {
       const { data } = heroDetails;
-      // console.log(heroDetails);
-      // console.log(heroDetails.data);
       
       if (data.error) {
         return <h1>Provide correct Hero ID</h1>;
       }
-      const fullInfo = Object.keys(data).map(detail => data[detail]);
+      console.log(data);
+      const getData = Object.keys(data).map(detail => data[detail]);
+      console.log(getData);
 
-      // console.log(fullInfo);
-
-
-      const { biography } = data;
-      // console.log(biography)
-      // const fullHeroDetails = Object.keys(biography).map(detail => <li key={detail}>{detail.toUpperCase()} : {biography[detail]} </li>);
-      // console.log(fullHeroDetails);
-      
-      setSearchHeroDetailsContent(fullInfo);
+      const fullInfo = () => {
+        for(const info of getData) {
+          console.log(info);
+          setSearchHeroDetailsContent(info);
+        }
+      }
+      fullInfo();
+     
       setLoadingState(false);
     })
   }, [id]);
+  
   console.log(searchHeroDetails);
   return (
     <>
     { !isLoading && (
       <section className='hero__details'>
+        
         <h1>Hero details:</h1>
-    <ul>{Object.keys(searchHeroDetails).map(info => <li>{searchHeroDetails[info]}</li>)}</ul>
-        {/* <ul>{searchHeroDetails.map(info => Object.keys(info).map(detal => console.log(`${info}: ${detal}`)))}</ul> */}
+        <>
+        {Object.keys(searchHeroDetails).map((key) => (
+          <div>
+            <img src={searchHeroDetails.url} alt='Super Hero'></img>
+          </div>
+          
+         ))
+        }
+        </>
       </section> 
-      
     )} {
       isLoading && <Loader />
     }
